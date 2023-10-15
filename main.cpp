bbb485fc897986c44104e8f986e68879c59182ee
bartlogut <116203748+bartlogut@users.noreply.github.com>
2023-10-15 20:55:54 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include "biblioteka.cpp"
+
+int main(){
+    cout << "Witaj";
+    cout << "Wybierz z poniższych liczb ta, która ci odpowiada"
+    cout << "1 - Dodawania";
+    cout << "2 - Odejmowania";
+    cout << "3 - Mnozenia";
+
+    int n;
+    cin >> n;
+
+    if (n==1)
+    dodawanie();
+    else
+
+    if(n==2)
+    odejmowanie();
+    else
+
+    if(n==3)
+    mnozenie();
+    else
+
+    cout << "Podales zle dane.";
+
+}
